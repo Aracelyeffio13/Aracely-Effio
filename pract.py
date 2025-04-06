d43f1d1b8b17c26d92e34ec8aa07a9b4fb81d98c
Aracelyeffio13 <allanizeffio04@gmail.com>
2025-04-05 19:19:05 -0500

    Add files via upload

---
+++ pract.py
@@ -0,0 +1,30 @@
+# hola mundo
+print("="*30)
+print("hola, mundo")
+print("="*30)
+print()
+#tipos de datos y variables
+
+nombre=" aracely " #esto es un string
+edad= 14 # esto es un int
+altura= 1.49# esto es un float
+es_estudiante = True #bool
+
+print( "mi nombre es ", nombre , "\n"
+      "mi edad es", edad , "\n"
+      "mi altura es", altura ,"\n"
+      "y soy estudiante" ,es_estudiante )
+print()
+print(type(nombre))
+print(type(edad))
+print(type(altura))
+print(type(es_estudiante))
+#operadores basicos
+
+a = 10 
+b = 5
+print(a + b) #esto es suma
+print(a - b) #esto es resta
+print(a * b)# esto es multiplicacion
+print(a / b)#esto es division
+print(a ** b)#esto es al cuadrado
